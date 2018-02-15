bed21533d26b865967100948db3fc8fa47c61194
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-15 13:59:21 -0800

    SecondActivity error resolved

--- app/src/main/java/com/vogella/android/espressofirst/SecondActivity.java
+++ app/src/main/java/com/vogella/android/espressofirst/SecondActivity.java
@@ -1,10 +1,14 @@
 package com.vogella.android.espressofirst;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
 /**
  * Created by kangsik_kevin_lee on 2/15/18.
  */
 
-public class SecondActivity {
+public class SecondActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
