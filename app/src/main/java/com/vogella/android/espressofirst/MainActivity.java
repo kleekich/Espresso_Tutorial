a0ed068e6c1c89c52bba203719c5c9c400101828
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-15 14:05:01 -0800

    MainActivity edited

--- app/src/main/java/com/vogella/android/espressofirst/MainActivity.java
+++ app/src/main/java/com/vogella/android/espressofirst/MainActivity.java
@@ -1,13 +1,36 @@
 package com.vogella.android.espressofirst;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.content.Intent;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.inputField);
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.changeText:
+                editText.setText("Lalala");
+                break;
+            case R.id.switchActivity:
+                Intent intent  = new Intent(this, SecondActivity.class);
+                intent.putExtra("input", editText.getText().toString());
+                startActivity(intent);
+                break;
+
+        }
     }
 }
